da5dfead25e1d19af61f171156f8d3f5782ea5e2
GermainDR <germain.doglioli-ruppert@etu.unice.fr>
2024-03-11 13:50:29 +0100

    Debut

---
+++ app/src/main/java/edu/dg202433/android_projet/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.dg202433.android_projet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
