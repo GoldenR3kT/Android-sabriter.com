ef6f9d3d066556130bb1070e05bf0b02566180c2
GermainDR <germain.doglioli-ruppert@etu.unice.fr>
2024-03-18 16:11:10 +0100

    maj

--- app/src/main/java/edu/dg202433/sabriter/MapActivity.java
+++ app/src/main/java/edu/dg202433/sabriter/MapActivity.java
@@ -1,8 +1,16 @@
 package edu.dg202433.sabriter;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceManager;
 
@@ -48,6 +56,40 @@ public class MapActivity extends AppCompatActivity {
                 items, new ItemizedOverlayWithFocus.OnItemGestureListener<OverlayItem>() {
                     @Override
                     public boolean onItemSingleTapUp(int index, OverlayItem item) {
+                        AlertDialog.Builder alertDialogBuilder;
+                        alertDialogBuilder = new AlertDialog.Builder(MapActivity.this);
+
+                        alertDialogBuilder.setTitle("titre");
+                        alertDialogBuilder.setIcon(R.drawable.home);
+
+                        ImageView imageView = new ImageView(MapActivity.this);
+                        imageView.setImageResource(R.drawable.tente_test1);
+
+
+                        imageView.setAdjustViewBounds(true);
+                        imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                        imageView.setMaxWidth(700);
+                        imageView.setMaxHeight(900);
+
+                        alertDialogBuilder.setView(imageView);
+
+
+                        alertDialogBuilder
+                                .setMessage("message")
+                                .setCancelable(true)
+                                .setNeutralButton("Voir l'offre", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        Intent intent = new Intent(MapActivity.this, HouseActivity.class);
+                                        startActivity(intent);
+                                    }
+                                });
+
+
+                        AlertDialog alertDialog = alertDialogBuilder.create();
+
+
+                        alertDialog.show();
                         return false;
                     }
 
@@ -59,10 +101,21 @@ public class MapActivity extends AppCompatActivity {
 
         mOverlay.setFocusItemsOnTap(true);
         map.getOverlays().add(mOverlay);
+
+
+        TextView title = findViewById(R.id.title);
+        title.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        });
     }
 
     @Override
     public void onPause() {
+
+
+
+
         super.onPause();
         map.onPause();
     }
